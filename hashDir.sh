65c61729941fd5a39da41c9c5e56dbb27cc54a4b
Thomas Dahlstrom <tdahlstrom@gmail.com>
2024-04-15 16:43:04 -0700

    Update hashDir.sh to current method of hashing.

--- hashDir.sh
+++ hashDir.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 dir=$1
-tarcmd=tar
-if [ "$(uname)" == "Darwin" ]; then
-    tarcmd=gtar
-fi
-if [ -z "$dir" ]; then
-    echo "Usage: $0 <dir>"
-    exit 1
-fi
-cmd="LC_ALL=C $tarcmd -C $dir -cf - --posix --pax-option=exthdr.name=%d/PaxHeaders/%f --pax-option=delete=atime,delete=ctime --mode='600' --sort=name --mtime='2015-10-21 00:00Z' --group=0 --owner=0 --numeric-owner . | sha256sum"
+
+cmd="LC_ALL=C tar -C $dir -cf - --posix --pax-option=exthdr.name=%d/PaxHeaders/%f --pax-option=delete=atime,delete=ctime --mode='go+u,go-w' --sort=name --clamp-mtime --mtime='2015-10-21 00:00Z' --group=0 --owner=0 --numeric-owner . | sha256sum"
 
 #echo $cmd
 eval $cmd | awk '{print $1}'
